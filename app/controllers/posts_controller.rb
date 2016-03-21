6380873b81ffa619faa33231cfd904948a953bb0
Mason Pierce & Michael Pintar <pair+masopie.thepintar@devbootcamp.com>
2016-03-21 14:40:22 -0500

    celebrate the small things

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,32 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def new
+  end
+
+  def create
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    @comments = @post.comments
+  end
+
+  def vote
+    @post = Post.find(params[:id])
+    @post.votes += 1
+    @post.save
+    redirect_to posts_path
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+
+  end
+
 end
